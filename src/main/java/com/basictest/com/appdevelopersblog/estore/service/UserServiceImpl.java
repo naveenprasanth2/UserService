019d6f56a795877af8d6975fcf955056f3279f89
naveenprasanth <developer.hotel@bookany.in>
2024-05-05 17:32:06 +0530

    refactored code

--- src/main/java/com/basictest/com/appdevelopersblog/estore/service/UserServiceImpl.java
+++ src/main/java/com/basictest/com/appdevelopersblog/estore/service/UserServiceImpl.java
@@ -7,6 +7,9 @@ import java.util.UUID;
 public class UserServiceImpl implements UserService {
     @Override
     public User createUser(String firstName, String lastName, String email, String password, String repeatPassword) {
+        if (firstName == null || firstName.isEmpty()) {
+            throw new IllegalArgumentException("Please enter the right name");
+        }
         return new User(firstName, lastName, email, password, repeatPassword, UUID.randomUUID().toString());
     }
 }
--- src/test/java/com/basictest/com/basictest/com/appdevelopersblog/estore/UserServiceTest.java
+++ src/test/java/com/basictest/com/basictest/com/appdevelopersblog/estore/UserServiceTest.java
@@ -4,26 +4,31 @@ import com.basictest.com.appdevelopersblog.estore.model.User;
 import com.basictest.com.appdevelopersblog.estore.service.UserService;
 import com.basictest.com.appdevelopersblog.estore.service.UserServiceImpl;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.*;
 
 class UserServiceTest {
     private UserService userService;
+    String firstName;
+    String lastName;
+    String email;
+    String password;
+    String repeatPassword;
 
     @BeforeEach
-    void beforeEach(){
+    void beforeEach() {
         userService = new UserServiceImpl();
+        firstName = "naveen";
+        lastName = "prasanth";
+        email = "naveen@gmail.com";
+        password = "12345678";
+        repeatPassword = "12345678";
     }
 
     @Test
     void testCreateUser_whenDetailsProvided_returnUserObject() {
-        String firstName = "naveen";
-        String lastName = "prasanth";
-        String email = "naveen@gmail.com";
-        String password = "12345678";
-        String repeatPassword = "12345678";
         User user = userService.createUser(firstName, lastName, email, password, repeatPassword);
         assertNotNull(user, "The user object is null");
         assertEquals(firstName, user.getFirstName(), "Checks whether both the names are same");
@@ -34,12 +39,17 @@ class UserServiceTest {
 
 //    @Test
 //    void testCreateUser_whenDetailsProvided_returnedUserObjectContainsTheSameName() {
-//        String firstName = "naveen";
-//        String lastName = "prasanth";
-//        String email = "naveen@gmail.com";
-//        String password = "12345678";
-//        String repeatPassword = "12345678";
 //        User user = userService.createUser(firstName, lastName, email, password, repeatPassword);
 //        assertEquals(firstName, user.getFirstName(), "Checks whether both the names are same");
 //    }
+
+    @DisplayName("Empty first name causes correct exception")
+    @Test
+    void testCreateUser_whenFirstNameIsEmpty_throwsIllegalArgumentException() {
+        firstName = "";
+        IllegalArgumentException illegalArgumentException = assertThrows(IllegalArgumentException.class,
+                () -> userService.createUser(firstName, lastName, email, password, repeatPassword),
+                "Empty first name shpuld throw illegal argument exception");
+        assertEquals("Please enter the right name", illegalArgumentException.getMessage());
+    }
 }
